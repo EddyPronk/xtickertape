14e6583a5e0caaf9746f06c3176a4be354513473
Ted Phelps <phelps@gnusto.com>
1998-10-29 03:40:25 +0000

    initial

---
+++ version.h
@@ -0,0 +1,13 @@
+/*
+ * $Id: version.h,v 1.1 1998/10/29 03:40:25 phelps Exp $
+ * Copyright!
+ *
+ * Merely contains the version number
+ */
+
+#ifdef ORBIT
+#define VERSION "1.4.4 (orbit)"
+#else /* ORBIT */
+#define VERSION "1.4.4"
+#endif /* ORBIT */
+
