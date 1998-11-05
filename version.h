6f6f2ddf15f140de35b726cf20715e770fe7988b
Ted Phelps <phelps@gnusto.com>
1998-11-05 01:55:30 +0000

    Upped the version number to indicate new functionality

--- version.h
+++ version.h
@@ -1,13 +1,13 @@
 /*
- * $Id: version.h,v 1.1 1998/10/29 03:40:25 phelps Exp $
+ * $Id: version.h,v 1.2 1998/11/05 01:55:30 phelps Exp $
  * Copyright!
  *
  * Merely contains the version number
  */
 
 #ifdef ORBIT
-#define VERSION "1.4.4 (orbit)"
+#define VERSION "1.5 (orbit)"
 #else /* ORBIT */
-#define VERSION "1.4.4"
+#define VERSION "1.5"
 #endif /* ORBIT */
 
