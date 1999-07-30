c6d9872c0e625fc633b73f4d6c286e0f470b1a74
Ted Phelps <phelps@gnusto.com>
1999-07-30 05:58:12 +0000

    Switched to the other style of multi-line C string constant

--- usenet.h
+++ usenet.h
@@ -27,64 +27,64 @@
 
 ****************************************************************/
 
-static char *defaultUsenetFile = "\
-# $HOME/.ticker/usenet -- Usenet subscription for Tickertape\n\
-#\n\
-# empty lines and those beginning with a hash are ignored\n\
-#\n\
-# each field after the group match is of the form:\n\
-#\n\
-#       <field> <op> <pattern>\n\
-#\n\
-# and seperated by slashes, `/'\n\
-#\n\
-# field is one of:\n\
-#       from \n\
-#       email\n\
-#       subject\n\
-#       keywords\n\
-#       x-posts (cross posts)\n\
-#\n\
-# op is one of:\n\
-#  matches -- regex\n\
-#      not -- does not match regex\n\
-#        = -- equals\n\
-#       != -- not equals\n\
-#        < -- less than (Cross posts only)\n\
-#        > -- greater than (Cross posts only)\n\
-#       <= -- less or equal (Cross posts only)\n\
-#       >= -- greater or equal (Cross posts only)\n\
-#\n\
-# pattern can be a number, a words (or words) or a regex depending on \n\
-# the operator\n\
-#\n\
-# The spaces between the <field>, <op> and the <pattern> ARE important\n\
-#\n\
-# Regex matches a NOT case sensitive\n\
-#\n\
-# Each expression on a single line is AND'ed together.  Different\n\
-# lines are the equivalent of OR'ing\n\
-#\n\
-# A few examples:\n\
-#\n\
-# All articles from Linus Torvalds\n\
-#.*/ from matches Linus\n\
-\n\
-# All articles from people from DSTC but not the ones in local groups\n\
-#not dstc.* / email matches .*dstc.edu.au\n\
-\n\
-# All articles in the DSTC announcement group\n\
-#dstc.announce\n\
-\n\
-# Articles in the computer groups about PGP, but not the ones heavily\n\
-# cross posted\n\
-#comp.* / subject matches PGP / x-posts < 5\n\
-\n\
-# All articles in forsale groups selling SPARCs posted by people in Oz\n\
-#.*forsale.* / subject matches SPARC / email matches .*au\n\
-\n\
-# Articles about Mitnick and from one of his friends\n\
-#.* / subject matches mitnick\n\
-#.* / email matches lewiz\n\
-#.* / from matches lewiz\n\
-";
+static char *defaultUsenetFile =
+"# $HOME/.ticker/usenet -- Usenet subscription for Tickertape\n"
+"#\n"
+"# empty lines and those beginning with a hash are ignored\n"
+"#\n"
+"# each field after the group match is of the form:\n"
+"#\n"
+"#       <field> <op> <pattern>\n"
+"#\n"
+"# and seperated by slashes, `/'\n"
+"#\n"
+"# field is one of:\n"
+"#       from \n"
+"#       email\n"
+"#       subject\n"
+"#       keywords\n"
+"#       x-posts (cross posts)\n"
+"#\n"
+"# op is one of:\n"
+"#  matches -- regex\n"
+"#      not -- does not match regex\n"
+"#        = -- equals\n"
+"#       != -- not equals\n"
+"#        < -- less than (Cross posts only)\n"
+"#        > -- greater than (Cross posts only)\n"
+"#       <= -- less or equal (Cross posts only)\n"
+"#       >= -- greater or equal (Cross posts only)\n"
+"#\n"
+"# pattern can be a number, a words (or words) or a regex depending on \n"
+"# the operator\n"
+"#\n"
+"# The spaces between the <field>, <op> and the <pattern> ARE important\n"
+"#\n"
+"# Regex matches a NOT case sensitive\n"
+"#\n"
+"# Each expression on a single line is AND'ed together.  Different\n"
+"# lines are the equivalent of OR'ing\n"
+"#\n"
+"# A few examples:\n"
+"#\n"
+"# All articles from Linus Torvalds\n"
+"#.*/ from matches Linus\n"
+"\n"
+"# All articles from people from DSTC but not the ones in local groups\n"
+"#not dstc.* / email matches .*dstc.edu.au\n"
+"\n"
+"# All articles in the DSTC announcement group\n"
+"#dstc.announce\n"
+"\n"
+"# Articles in the computer groups about PGP, but not the ones heavily\n"
+"# cross posted\n"
+"#comp.* / subject matches PGP / x-posts < 5\n"
+"\n"
+"# All articles in forsale groups selling SPARCs posted by people in Oz\n"
+"#.*forsale.* / subject matches SPARC / email matches .*au\n"
+"\n"
+"# Articles about Mitnick and from one of his friends\n"
+"#.* / subject matches mitnick\n"
+"#.* / email matches lewiz\n"
+"#.* / from matches lewiz\n";
+
