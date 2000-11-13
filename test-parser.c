b468f10353bf83825cc8f5a171f3616880f96019
Ted Phelps <phelps@gnusto.com>
2000-11-13 08:07:03 +0000

    Implemented the `+' primitive

--- test-parser.c
+++ test-parser.c
@@ -507,8 +507,167 @@ static int prim_lambda(env_t env, sexp_t args, sexp_t *result, elvin_error_t err
 /* The `+' primitive function */
 static int prim_plus(env_t env, sexp_t args, sexp_t *result, elvin_error_t error)
 {
-    ELVIN_ERROR_ELVIN_NOT_YET_IMPLEMENTED(error, "prim_plus");
-    return 0;
+    sexp_t sum;
+
+    /* Start with a sum of zero */
+    if ((sum = int32_alloc(0, error)) == NULL)
+    {
+	return 0;
+    }
+
+    /* Keep going until we run out of args */
+    while (sexp_get_type(args) == SEXP_CONS)
+    {
+	sexp_t value;
+
+	/* Evaluate the car */
+	if (sexp_eval(cons_car(args, error), env, &value, error) == 0)
+	{
+	    sexp_free(value, NULL);
+	    return 0;
+	}
+
+	/* Figure out what to do based on the type of the first arg */
+	switch (sexp_get_type(sum))
+	{
+	    case SEXP_INT32:
+	    {
+		int32_t lhs = int32_value(sum, error);
+
+		switch (sexp_get_type(value))
+		{
+		    case SEXP_INT32:
+		    {
+			*result = int32_alloc(lhs + int32_value(value, error), error);
+			break;
+		    }
+
+		    case SEXP_INT64:
+		    {
+			*result = int64_alloc(lhs + int64_value(value, error), error);
+			break;
+		    }
+
+		    case SEXP_FLOAT:
+		    {
+			*result = float_alloc(lhs + float_value(value, error), error);
+			break;
+		    }
+
+		    default:
+		    {
+			ELVIN_ERROR_ELVIN_NOT_YET_IMPLEMENTED(error, "bad type");
+			*result = NULL;
+		    }
+		}
+
+		break;
+	    }
+
+	    case SEXP_INT64:
+	    {
+		int64_t lhs = int64_value(sum, error);
+
+		switch (sexp_get_type(value))
+		{
+		    case SEXP_INT32:
+		    {
+			*result = int64_alloc(lhs + int32_value(value, error), error);
+			break;
+		    }
+
+		    case SEXP_INT64:
+		    {
+			*result = int64_alloc(lhs + int64_value(value, error), error);
+			break;
+		    }
+
+		    case SEXP_FLOAT:
+		    {
+			*result = float_alloc(lhs + float_value(value, error), error);
+			break;
+		    }
+
+		    default:
+		    {
+			ELVIN_ERROR_ELVIN_NOT_YET_IMPLEMENTED(error, "bad type");
+			*result = NULL;
+		    }
+		}
+
+		break;
+	    }
+
+	    case SEXP_FLOAT:
+	    {
+		double lhs = float_value(sum, error);
+
+		switch (sexp_get_type(value))
+		{
+		    case SEXP_INT32:
+		    {
+			*result = float_alloc(lhs + int32_value(value, error), error);
+			break;
+		    }
+
+		    case SEXP_INT64:
+		    {
+			*result = float_alloc(lhs + int64_value(value, error), error);
+			break;
+		    }
+
+		    case SEXP_FLOAT:
+		    {
+			*result = float_alloc(lhs + float_value(value, error), error);
+			break;
+		    }
+
+		    default:
+		    {
+			ELVIN_ERROR_ELVIN_NOT_YET_IMPLEMENTED(error, "bad type");
+			*result = NULL;
+		    }
+		}
+
+		break;
+	    }
+
+	    default:
+	    {
+		ELVIN_ERROR_ELVIN_NOT_YET_IMPLEMENTED(error, "bad type");
+		*result = NULL;
+		break;
+	    }
+	}
+
+	/* Free the value */
+	if (sexp_free(value, error) == 0)
+	{
+	    return 0;
+	}
+
+	/* Check for an error */
+	if (*result == NULL)
+	{
+	    return 0;
+	}
+
+	/* The result becomes the sum */
+	sum = *result;
+
+	/* Move on to the next arg */
+	args = cons_cdr(args, error);
+    }
+
+    /* Make sure we end cleanly */
+    if (sexp_get_type(args) != SEXP_NIL)
+    {
+	ELVIN_ERROR_ELVIN_NOT_YET_IMPLEMENTED(error, "bad arg list");
+	return 0;
+    }
+
+    *result = sum;
+    return 1;
 }
 
 /* The `quote' primitive function */
