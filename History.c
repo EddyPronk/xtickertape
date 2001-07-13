3b54a185fd6a3285149bc6ae0fe89430a1b8b7a1
Ted Phelps <phelps@gnusto.com>
2001-07-13 13:17:39 +0000

    Moved the public methods to the bottom, moved the interesting bits of HistoryAddMessage() into insert_message() to separate the logistics of updating the screen from the difficulties of threading.  Still not coping with inserts before the end, but it's getting better.

--- History.c
+++ History.c
@@ -28,7 +28,7 @@
 ****************************************************************/
 
 #ifndef lint
-static const char cvsid[] = "$Id: History.c,v 1.10 2001/07/13 08:48:57 phelps Exp $";
+static const char cvsid[] = "$Id: History.c,v 1.11 2001/07/13 13:17:39 phelps Exp $";
 #endif /* lint */
 
 #ifdef HAVE_CONFIG_H
@@ -133,10 +133,10 @@ static XtResource resources[] =
 	offset(history.margin_height), XtRImmediate, (XtPointer)5
     },
 
-    /* unsigned int message_count */
+    /* unsigned int message_capacity */
     {
-	XtNmessageCount, XtCMessageCount, XtRDimension, sizeof(unsigned int),
-	offset(history.message_count), XtRImmediate, (XtPointer)32
+	XtNmessageCapacity, XtCMessageCapacity, XtRDimension, sizeof(unsigned int),
+	offset(history.message_capacity), XtRImmediate, (XtPointer)32
     }
 };
 #undef offset
@@ -192,9 +192,6 @@ static XtGeometryResult query_geometry(
     Widget self,
     XtWidgetGeometry *intended,
     XtWidgetGeometry *preferred);
-static void update_scrollbars(Widget widget);
-static void paint(HistoryWidget self, XRectangle *bbox);
-
 
 /*
  *
@@ -304,140 +301,6 @@ static void set_origin(HistoryWidget self, long x, long y)
     self -> history.x = x;
     self -> history.y = y;
 }
-
-
-/*
- *
- * Method definitions
- *
- */
-
-/* Set the widget's display to be threaded or not */
-void HistorySetThreaded(Widget widget, Boolean is_threaded)
-{
-    dprintf(("HistorySetThreaded(): not yet implemented\n"));
-}
-
-/* Returns whether or not the history is threaded */
-Boolean HistoryIsThreaded(Widget widget)
-{
-    dprintf(("HistoryIsThreaded(): not yet implemented\n"));
-    return False;
-}
-
-/* Set the widget's display to show timestamps or not */
-void HistorySetShowTimestamps(Widget widget, Boolean show_timestamps)
-{
-    dprintf(("HistorySetShowTimestamps(): not yet implemented\n"));
-}
-
-/* Returns whether or not the timestamps are visible */
-Boolean HistoryIsShowingTimestamps(Widget widget)
-{
-    dprintf(("HistoryIsShowingTimestamps(): not yet implemented\n"));
-    return False;
-}
-
-
-/* Adds a new message to the History */
-void HistoryAddMessage(Widget widget, message_t message)
-{
-    HistoryWidget self = (HistoryWidget)widget;
-    message_view_t view, old;
-    struct string_sizes sizes;
-    long width, height;
-    int flag = 0;
-    unsigned int i;
-    XRectangle bbox;
-
-    /* FIX THIS: this is fairly bogus */
-
-    /* If there's a first view then free it */
-    if (self -> history.message_views[0])
-    {
-	message_view_free(self -> history.message_views[0]);
-	flag = 1;
-    }
-
-    /* Recompute the widget's width and height and shift everything up */
-    width = 0;
-    height = 0;
-    for (i = 1; i < self -> history.message_count; i++)
-    {
-	if ((view = self -> history.message_views[i]) != NULL)
-	{
-	    /* Measure the view */
-	    message_view_get_sizes(view, &sizes);
-
-	    /* Update our dimensions */
-	    width = MAX(width, sizes.width);
-	    height += sizes.ascent + sizes.descent;
-	}
-
-	/* Bump it up one position */
-	self -> history.message_views[i - 1] = view;
-    }
-
-    /* Create a message view for the message */
-    view = message_view_alloc(message, self -> history.font);
-    self -> history.message_views[self -> history.message_count - 1] = view;
-
-    /* Adjust the width and height one last time */
-    message_view_get_sizes(view, &sizes);
-    width = MAX(width, sizes.width);
-    height += sizes.ascent + sizes.descent;
-
-    /* Record our new measurements */
-    self -> history.width = width + (long)self -> history.margin_width * 2;
-    self -> history.height = height + (long)self -> history.margin_height * 2;
-
-    /* Update the widget's scrollbars */
-    update_scrollbars(widget);
-
-    /* Bail if the widget hasn't been realized */
-    if (self -> history.gc == None)
-    {
-	return;
-    }
-
-    /* Figure out what needs to be repainted */
-    /* FIX THIS: we should be able to do a CopyArea trick here */
-    bbox.x = 0;
-    bbox.y = 0;
-    bbox.width = self -> core.width;
-    bbox.height = self -> core.height;
-
-    /* Repaint the widget */
-    paint(self, &bbox);
-}
-
-/* Kills the thread of the given message */
-void HistoryKillThread(Widget widget, message_t message)
-{
-    dprintf(("HistoryKillThread(): not yet implemented\n"));
-}
-
-/* Returns whether or not a given message is in a killed thread */
-Boolean HistoryIsMessageKilled(Widget widget, message_t message)
-{
-    dprintf(("HistoryIsMessageKilled(): not yet implemented\n"));
-    return False;
-}
-
-/* Selects a message in the history */
-void HistorySelect(Widget widget, message_t message)
-{
-    dprintf(("HistorySelect(): not yet implemented\n"));
-}
-
-/* Returns the selected message or NULL if none is selected */
-message_t HistoryGetSelection(Widget widget)
-{
-    dprintf(("HistoryGetSelection(): not yet implemented\n"));
-    return NULL;
-}
-
-
 
 /* Callback for the vertical scrollbar */
 static void vert_drag_cb(Widget widget, XtPointer client_data, XtPointer call_data)
@@ -581,8 +444,9 @@ static void init(Widget request, Widget widget, ArgList args, Cardinal *num_args
     self -> history.dqueue = NULL;
 
     /* Allocate enough room for all of our message views */
+    self -> history.message_count = 0;
     self -> history.message_views =
-	calloc(self -> history.message_count, sizeof(message_view_t));
+	calloc(self -> history.message_capacity, sizeof(message_view_t));
 
     /* Create the horizontal scrollbar */
     scrollbar = XtVaCreateManagedWidget(
@@ -771,7 +635,7 @@ static void paint(HistoryWidget self, XRectangle *bbox)
     y = ymargin - self -> history.y;
 
     /* Draw all visible message views */
-    for (i = 0; i < self -> history.message_count; i++)
+    for (i = 0; i < self -> history.message_capacity; i++)
     {
 	/* Skip NULL message views */
 	if ((view = self -> history.message_views[i]) == NULL)
@@ -858,6 +722,138 @@ static void gexpose(Widget widget, XtPointer rock, XEvent *event, Boolean *ignor
     }
 }
 
+/* Insert a message before the given index */
+static void insert_message(HistoryWidget self, unsigned int index, message_t message)
+{
+    Display *display = XtDisplay((Widget)self);
+    Window window = XtWindow((Widget)self);
+    message_view_t view;
+    struct string_sizes sizes;
+    long y;
+    long width = 0;
+    long height = 0;
+    XRectangle bbox;
+    unsigned int i;
+    XGCValues values;
+    GC gc = self -> history.gc;
+
+    /* Sanity check */
+    assert(index <= self -> history.message_count);
+
+    /* Cancel our clip mask */
+    if (gc != None)
+    {
+	values.clip_mask = None;
+/* 	values.foreground = WhitePixelOfScreen(XtScreen((Widget)self)); */
+	values.foreground = 0xc0f0c0;
+	XChangeGC(display, gc, GCClipMask | GCForeground, &values);
+    }
+
+    /* Are we out of room? */
+    if (! (self -> history.message_count < self -> history.message_capacity))
+    {
+	/* Get rid of the first message and scroll the others upwards */
+	view = self -> history.message_views[0];
+	message_view_get_sizes(view, &sizes);
+	message_view_free(view);
+
+	/* Figure out where to start copying */
+	y = sizes.ascent + sizes.descent;
+
+	/* Move the other views towards the beginning of the array */
+	for (i = 1; i < index; i++)
+	{
+	    view = self -> history.message_views[i];
+
+	    /* Measure the view for the new width/height of the widget */
+	    message_view_get_sizes(view, &sizes);
+	    width = MAX(width, sizes.width);
+	    height += sizes.ascent + sizes.descent;
+	    self -> history.message_views[i - 1] = view;
+	}
+
+	/* Move the messages upwards as a block */
+	if (gc != None)
+	{
+	    XCopyArea(
+		display, window, window, gc,
+		0, self -> history.margin_height - self -> history.y + y,
+		self -> core.width, height,
+		0, self -> history.margin_height - self -> history.y);
+	}
+
+	/* The index gets decremented */
+	index--;
+    }
+    else
+    {
+	/* Otherwise simply measure the messages */
+	for (i = 0; i < index; i++)
+	{
+	    message_view_get_sizes(self -> history.message_views[i], &sizes);
+	    width = MAX(width, sizes.width);
+	    height += sizes.ascent + sizes.descent;
+	}
+
+	/* Update the counter */
+	/* FIX THIS: perhaps do this at the end? */
+	self -> history.message_count++;
+    }
+
+    /* Create a new message_view */
+    view = message_view_alloc(message, self -> history.font);
+    self -> history.message_views[index] = view;
+
+    /* Measure it */
+    message_view_get_sizes(view, &sizes);
+
+    /* Draw it if we have a graphics context */
+    if (gc != None)
+    {
+	/* Erase the space needed by the message */
+	XFillRectangle(
+	    display, window, gc,
+	    0, self -> history.margin_height + height - self -> history.y,
+	    self -> core.width, sizes.ascent + sizes.descent);
+
+	/* Paint it */
+	bbox.x = 0;
+	bbox.y = self -> history.margin_height + height - self -> history.y;
+	bbox.width = self -> core.width;
+	bbox.height = sizes.ascent = sizes.descent;
+
+	message_view_paint(
+	    view, display, window, gc,
+	    self -> history.group_pixel, self -> history.user_pixel,
+	    self -> history.string_pixel, self -> history.separator_pixel,
+	    self -> history.margin_width - self -> history.x,
+	    self -> history.margin_height - self -> history.y + height +
+	    self -> history.font -> ascent,
+	    &bbox);
+    }
+
+    /* Update the width and height */
+    width = MAX(width, sizes.width);
+    height += sizes.ascent + sizes.descent;
+
+    /* FIX THIS: copy the rest down! */
+
+    /* Measure the remaining items */
+    for (i = index + 1; i < self -> history.message_count; i++)
+    {
+	message_view_get_sizes(self -> history.message_views[i], &sizes);
+	width = MAX(width, sizes.width);
+	height += sizes.ascent + sizes.descent;
+    }
+
+    /* Update the widget's dimensions */
+    self -> history.width = width + (long)self -> history.margin_width * 2;
+    self -> history.height = height + (long)self -> history.margin_height * 2;
+
+    /* Update the scrollbars */
+    update_scrollbars((Widget)self);
+}
+
 
 /* Destroy the widget */
 static void destroy(Widget self)
@@ -896,3 +892,76 @@ static XtGeometryResult query_geometry(
     dprintf(("History: query_geometry()\n"));
     return XtGeometryYes;
 }
+
+
+
+/*
+ *
+ * Method definitions
+ *
+ */
+
+/* Set the widget's display to be threaded or not */
+void HistorySetThreaded(Widget widget, Boolean is_threaded)
+{
+    dprintf(("HistorySetThreaded(): not yet implemented\n"));
+}
+
+/* Returns whether or not the history is threaded */
+Boolean HistoryIsThreaded(Widget widget)
+{
+    dprintf(("HistoryIsThreaded(): not yet implemented\n"));
+    return False;
+}
+
+/* Set the widget's display to show timestamps or not */
+void HistorySetShowTimestamps(Widget widget, Boolean show_timestamps)
+{
+    dprintf(("HistorySetShowTimestamps(): not yet implemented\n"));
+}
+
+/* Returns whether or not the timestamps are visible */
+Boolean HistoryIsShowingTimestamps(Widget widget)
+{
+    dprintf(("HistoryIsShowingTimestamps(): not yet implemented\n"));
+    return False;
+}
+
+
+/* Adds a new message to the History */
+void HistoryAddMessage(Widget widget, message_t message)
+{
+    HistoryWidget self = (HistoryWidget)widget;
+
+    /* For now just insert at the end */
+    insert_message(self, self -> history.message_count, message);
+}
+
+/* Kills the thread of the given message */
+void HistoryKillThread(Widget widget, message_t message)
+{
+    dprintf(("HistoryKillThread(): not yet implemented\n"));
+}
+
+/* Returns whether or not a given message is in a killed thread */
+Boolean HistoryIsMessageKilled(Widget widget, message_t message)
+{
+    dprintf(("HistoryIsMessageKilled(): not yet implemented\n"));
+    return False;
+}
+
+/* Selects a message in the history */
+void HistorySelect(Widget widget, message_t message)
+{
+    dprintf(("HistorySelect(): not yet implemented\n"));
+}
+
+/* Returns the selected message or NULL if none is selected */
+message_t HistoryGetSelection(Widget widget)
+{
+    dprintf(("HistoryGetSelection(): not yet implemented\n"));
+    return NULL;
+}
+
+
+
